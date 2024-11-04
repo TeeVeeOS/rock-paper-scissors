88d10e33bc044096f712439ae20b6d6de992b372
TV <github@teeveeos.com>
2024-11-04 12:33:51 -0600

    Add playRound function to handle game logic for one round and update scores

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function getHumanChoice(){
     return(input.toLowerCase())
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    
+    if (humanChoice == computerChoice){
+        console.log('Tie!')
+    }
+    else if (
+        (humanChoice == 'rock' && computerChoice == 'scissors') ||
+        (humanChoice == 'scissors' && computerChoice == 'paper') ||
+        (humanChoice == 'paper' && computerChoice == 'rock') 
+    ){
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+    }
+    else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
